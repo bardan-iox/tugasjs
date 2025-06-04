d91b02690f6774334c65ef0b967854cd6bd5b694
bardan-iox <bardansulaiman123@gmail.com>
2025-06-04 07:12:43 +0700

    Delete tugasjs directory

--- tugasjs/Bab 5/cbagianA.js
+++
@@ -1,8 +0,0 @@
-var total_belanja = 1500000
-let discount;
-if(total_belanja >1000000) {
-    discount = 0.10;
-}
-var hasil = total_belanja-(total_belanja*discount)
-console.log()
-document.getElementById('hasil').innerText = hasil
